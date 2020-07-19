00fa6e758b54843657e97ea15b87803645eb7458
9631kunn <infourbh+1@gmail.com>
2020-07-19 16:34:10 +0900

    fix

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// materializeJS
+document.addEventListener("DOMContentLoaded", function () {
+  var sideNav = document.querySelectorAll(".sidenav");
+  var fixedBtn = document.querySelectorAll(".fixed-action-btn");
+  var modal = document.querySelectorAll(".modal");
+  var collapsible = document.querySelectorAll(".collapsible");
+  var carousel = document.querySelectorAll(".carousel");
+  var carousel2 = document.querySelectorAll(".carousel-slider");
+  var imagebox = document.querySelectorAll(".materialboxed");
+  var tab = document.querySelectorAll(".tabs");
+  var instancesNav = M.Sidenav.init(sideNav);
+  var instancesFixedBtn = M.FloatingActionButton.init(fixedBtn);
+  var instancesModal = M.Modal.init(modal);
+  var instancesCollaps = M.Collapsible.init(collapsible);
+  var instancesCarousel = M.Carousel.init(carousel);
+  var instancesCarousel2 = M.Carousel.init(carousel2, {
+    fullWidth: true,
+    indicators: true,
+  });
+  var instancesImagebox = M.Materialbox.init(imagebox);
+  var instancesTab = M.Tabs.init(tab);
+});
